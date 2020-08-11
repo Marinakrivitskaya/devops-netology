7726b2afa3e5c5831e6c1d77f178481aa0f65de6
Andrey Krylov <syatihoko@mail.ru>
2020-08-11 22:28:54 +0300

    14_05-script-02-py ready2

--- HomeWorks/14_04-script-02-py-6.py
+++ HomeWorks/14_04-script-02-py-6.py
@@ -1,7 +1,6 @@
 # -*- coding: utf8 -*-
 import os
 import sys
-import time
 
 #https://github.com/PyGithub/PyGithub
 from github import Github
@@ -10,20 +9,53 @@ from github import Github
 #https://pygithub.readthedocs.io/en/latest/examples/PullRequest.html
 #https://pygithub.readthedocs.io/en/latest/examples.html
 
+
+if len (sys.argv) > 1:
+    pr_body=sys.argv[1]
+    print("pr_body= ",pr_body)
+else:
+    raise BaseException ("Not enough arguments!")
+
+#Переходим в каталог репозитория
+os.popen("cd C:\\Users\\kaa\\PycharmProjects\\DevOps\\devops-netology\\HomeWorks")
+print(os.getcwd())
+#print(os.popen("git branch conf-merge"))
+#print(os.popen("git checkout  conf-merge"))
+print(os.popen("git checkout -b conf-merge"))
+
+
+#Подключаемся используя Токен
 g = Github("7523e782b42ba44b11a6ab1eb7ca364c576cdd0a")
-g = Github(base_url="https://api.github.com", login_or_token="7523e782b42ba44b11a6ab1eb7ca364c576cdd0a")
+g = Github(base_url="https://api.github.com", login_or_token="4cdd014c69f4c00aeb36e5eea002eb5e02950ac3")
+
 
 # Then play with your Github objects:
 
-
+#Просматриваем все репозитории
 for repo in g.get_user().get_repos():
     print(repo.name)
 
+#Выбираем репозиторий
 repo = g.get_user().get_repo("devops-netology")
 print(repo)
 
+# Тело для Pull Request
+body = '''
+SUMMARY
+Change HTTP library used to send requests
+
+TESTS
+  - [x] Send 'GET' request
+  - [x] Send 'POST' request with/without body
+'''
+
+#Делаем Pull Request
+#pr = repo.create_pull(title="Use 'requests' instead of 'httplib'", body=body, head="fix", base="master")
 
 
+
+#Просмотреть список
+#print(list(repo.get_branches()))
 
 #Create a new Pull Request
 #>>> repo = g.get_repo("PyGithub/PyGithub")
