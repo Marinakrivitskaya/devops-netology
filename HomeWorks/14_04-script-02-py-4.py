437c46f669712a410ec7f835643e6341e80523ce
Andrey Krylov <syatihoko@mail.ru>
2020-08-11 09:03:31 +0300

    14_04_script-04-python in Process

--- HomeWorks/14_04-script-02-py-4.py
+++ HomeWorks/14_04-script-02-py-4.py
@@ -12,18 +12,19 @@ site_dict = {}
 def check_list_of_sites(site_list, site_dict):
 
     for site_url in site_list:
-        print('site_url=', site_url )
+        #print('site_url=', site_url )
         site_dict=check_site_dns(site_url, site_dict)
     return site_dict
 
 
 def check_site_dns(site_url, site_dict):
     #site_dict = site_dict
-    site_url = site_url
+    #site_url = site_url
 
-    result_os = os.popen(f'dig +short {site_url} | grep  -E \'[0-9]\' | head -n 1')
+    result_os = os.popen(f'dig +short {site_url} | grep  -E \'[0-9]\' | head -n 1 | tr -d \'\r\n\'')
     for result in result_os:
         site_new_ip = result
+        #print('site_new_ip: ', site_new_ip)
         #print('site_new_ip= ', site_new_ip)
 
 
@@ -32,7 +33,7 @@ def check_site_dns(site_url, site_dict):
         #print(site_dict)
         #print('site_dict.get = ', site_dict.get(site_url))
         site_old_ip = site_dict[site_url]
-        print('site_old_ip: ', site_old_ip)
+        #print('site_old_ip: ', site_old_ip)
 
         if site_old_ip == site_new_ip:
             #print('YES2')
@@ -49,7 +50,7 @@ def check_site_dns(site_url, site_dict):
 while True:
     #site_dict = check_site_dns('google.com', site_dict)
     site_dict = check_list_of_sites(site_list, site_dict)
-    time.sleep(5)
+    time.sleep(2)
 else:
     print('Not Work')
 
